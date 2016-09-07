6591dc8367807514ac7bcf750fc4e8a490da7b58
tito moabi <david1@davids-macbook-pro.home>
2016-09-07 19:52:05 +0200

    footer display

--- themes/_o/footer.php
+++ themes/_o/footer.php
@@ -9,15 +9,6 @@
  * @since Twenty Fifteen 1.0
  */
 ?>
-
-<div id="footer-newsletter">
-	<div class="inner">
-		 <div class="pure-u-1" id="newsletter-form">
-		    <?php echo do_shortcode('[contact-form-7 id="1089" title="Newsletter"]'); ?>
-	    </div>
-	</div>
-</div>
-	   
 <footer id="colophon" class="site-footer" role="contentinfo">
   <div class="site-info">
     <div class="inner">
@@ -40,7 +31,7 @@
 </div><!-- site-wrapper -->
     <div class="infos"> 
 	    <div class="inner">
-          <span class="footer-title">Onlyoo</span> © Copyright <?php the_time('Y'); ?> Onlyooo. Tous droits réservés.
+          <span class="footer-title">Onlyoo</span> © Copyright <?php the_time('Y'); ?> <?php get_bloginfo('name'); ?>. <?php _e('Tous droits réservés.','online-booking'); ?>
 	    </div>
 	   
       </div>
@@ -49,21 +40,13 @@
 
 <?php
 $gaCode = get_field('code_ga','option');
-$prodUrl = get_field('url_de_production','option');
-//only echo analytics if if the good server
-$rootUrl= "https://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-if($rootUrl == $prodUrl){
+if($gaCode){
   echo $gaCode;
-} else {
-  echo '<div id="testWebsite">TEST</div>';
 }
-?>
-<!--[if lt IE 9]>
-<script type='text/javascript' src='//cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.js?ver=4.1.2'></script>
-<script type='text/javascript' src='//cdn.jsdelivr.net/respond/1.4.2/respond.min.js?ver=4.1.2'></script>
-<![endif]-->
 
-<link rel='stylesheet' id='fa'  href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' type='text/css' media='all' />
+?>
 
 </body>
 </html>
+
+<link rel='stylesheet' id='fa'  href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' type='text/css' media='all' />
