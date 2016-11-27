670d3d1d2a3cb119fbd417893033e61b9b468495
david Fieffe <moabi31@gmail.com>
2016-11-27 21:19:00 +0100

    filtering post

--- plugins/online-booking/public/partials/dashboard-manager-running-orders.php
+++ plugins/online-booking/public/partials/dashboard-manager-running-orders.php
@@ -32,6 +32,7 @@ $output .= '<h2> <i class="fa fa-clock-o" aria-hidden="true"></i> Réservations 
  * @return mixed|string
  */
 
+/*
 function my_posts_where_vendor_activities( $where ) {
 
 	$where = str_replace("meta_key = 'day_%", "meta_key LIKE 'day_%", $where);
@@ -41,6 +42,7 @@ function my_posts_where_vendor_activities( $where ) {
 }
 
 add_filter('posts_where', 'my_posts_where_vendor_activities');
+*/
 
 // args
 $vendor_posts = $ob_user->get_vendor_activities_ids($user_id);
