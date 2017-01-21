3ef916454a6c868f98bf02a17d1352008b534d95
david Fieffe <moabi31@gmail.com>
2017-01-21 11:51:58 +0100

    pending msg

--- plugins/online-booking/public/partials/vendor/vendor-list-packages.php
+++ plugins/online-booking/public/partials/vendor/vendor-list-packages.php
@@ -57,20 +57,31 @@ if ( $the_query->have_posts() ) {
 		$post_author = get_post_field( 'post_author', $post_id );
 		$output .= '<div class="block block-trip-container pure-u-1 pure-u-md-1-3 ">';
 		$output .= '<div class="block-trip card padd-l">';
-		$output .= '<h4><a href="';
-		$output .= get_the_permalink() . '" >';
+		$output .= '<h4>';
+		if($status == 'publish'){
+			$output .= '<a href="';
+			$output .= get_the_permalink() . '" >';
+		}
+
 		$output .= get_the_title();
 		if($status == 'pending'){
 			$output .= ' (En attente de validation)';
 		}
-		$output .= '</a></h4>';
+		if($status == 'publish'){
+			$output .= '</a>';
+		}
+		
+		$output .= '</h4>';
 		if(has_post_thumbnail()){
 			$output .= get_the_post_thumbnail(null, 'thumbnail');
 		} else {
 			$output .= '<img src="'.get_wp_attachment_filter_plugin_uri().'/public/img/sejour-placeholder.gif" alt="onlyoo - '.get_the_title().'"/>';
 		}
 		$output .= '<a target="_blank" class="loadit" href="'.home_url('dashboard/ajouter-un-programme?edit=').get_the_ID().'">Modifier</a>';
-		$output .= '<a target="_blank" class="seeit" href="'.get_the_permalink().'">Voir</a>';
+		if($status == 'publish'){
+			$output .= '<a target="_blank" class="seeit" href="'.get_the_permalink().'">Voir</a>';
+		}
+
 		$output .= '</div>';
 		$output .= '</div>';
 
